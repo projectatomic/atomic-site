eda33f3b6199352f049aea9662fa2ac58d2205f8
Tomas Tomecek <ttomecek@redhat.com>
2015-09-09 14:33:24 +0200

    setup: update dependencies

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 git submodule init && git submodule update
 
-sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch
+sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch make tar xz-devel zlib-devel libxml2-devel
 
 bundle install

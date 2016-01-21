d2aa660e64db278d10427dd56245faf00d1df811
Joe Brockmeier <jzb@zonker.net>
2016-01-21 14:11:32 -0500

    Adding a few packages that Middleman requires that weren't present, specifically ImageMagic and the rpm-config thingy.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 git submodule init && git submodule update
 
-sudo yum install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch make tar xz-devel zlib-devel libxml2-devel
+sudo dnf install -y ruby-devel rubygems-devel gcc-c++ curl-devel rubygem-bundler patch make tar xz-devel zlib-devel libxml2-devel ImageMagick redhat-rpm-config
 
 bundle install
